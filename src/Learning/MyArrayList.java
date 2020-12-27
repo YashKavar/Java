879c1d73d8a7a4b4f6c44b2f3378b315bdbfbab8
Yash Kavar <yashkavar05062001@gmail.com>
2020-12-27 11:42:15 +0530

    Learning file added.

---
+++ src/Learning/MyArrayList.java
@@ -0,0 +1,13 @@
+package Learning;
+
+import java.util.ArrayList;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+//        ArrayList<String> names = new ArrayList();
+//        names.add("Yash");
+//        System.out.println(names);
+        Pair<String, Integer> p1 = new Pair("Yash", 6046);
+        Pair<Boolean, String> p2 = new Pair(true, "True");
+    }
+}
---
+++ src/Learning/Pair.java
@@ -0,0 +1,10 @@
+package Learning;
+
+public class Pair<X, Y> {
+    X x;
+    Y y;
+    public Pair(X x, Y y){
+        this.x = x;
+        this.y = y;
+    }
+}
